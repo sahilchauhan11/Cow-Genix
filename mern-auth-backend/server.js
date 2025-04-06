5f91c308f99f07cf06be85ec807ea0fa713c9cb9
sahilchauhan11 <sahilsingh982167@gmail.com>
2025-04-06 22:22:50 +0530

    added cors

--- mern-auth-backend/server.js
+++ mern-auth-backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
 app.use(cookieParser());
 app.use(session({ secret: 'secretkey', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
