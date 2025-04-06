f133658a1a5c1429e6050236167c66a67b3c6a40
sahilchauhan11 <sahilsingh982167@gmail.com>
2025-04-06 17:01:39 +0530

    added VetNavbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,31 @@
 // src/components/Navbar.js
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="fixed bottom-0 w-full bg-white p-4 flex justify-around shadow-md">
-      <Link to="/home" className="text-black">Dashboard</Link>
+      <NavLink to="/home" className={({ isActive }) =>
+            isActive
+              ? "text-blue-600 font-semibold"
+              : "text-gray-500 hover:text-blue-500"
+          }>Dashboard</NavLink>
       
-      <Link to="/profile" className="text-gray-500">Profile</Link>
-      <Link to="/alertPage" className="text-gray-500">Alert</Link>
-      <Link to="/vets" className="text-gray-500">vet</Link>
+      <NavLink to="/profile" className={({ isActive }) =>
+            isActive
+              ? "text-blue-600 font-semibold"
+              : "text-gray-500 hover:text-blue-500"
+          }>Profile</NavLink>
+      <NavLink to="/alertPage" className={({ isActive }) =>
+            isActive
+              ? "text-blue-600 font-semibold"
+              : "text-gray-500 hover:text-blue-500"
+          }>Alert</NavLink>
+      <NavLink to="/vets" className={({ isActive }) =>
+            isActive
+              ? "text-blue-600 font-semibold"
+              : "text-gray-500 hover:text-blue-500"
+          }>vet</NavLink>
     </nav>
   );
 };
